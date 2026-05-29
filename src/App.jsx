baf87e83e9dc30326327c160fd766abfc40ff1bc
Araigoso <61702376+Araigoso@users.noreply.github.com>
2026-05-29 19:39:04 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -101,12 +101,13 @@ function Home() {
   return (
     <div className="app">
       <div className='titulo'>
-        <h1>SORTEO ❤️</h1>
+        <h1> SORTEO ❤️</h1>
 
       <p>
       Elegí tu número, participá y ayudanos a seguir latiendo  🌟
+      Recordá que podes elegir del 1 al 500
       </p>  
-      <p>Recordá que podes elegir del 1 al 500 </p>
+    
       </div>
 
       <div className="container">
